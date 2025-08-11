14df21a947abd83bb5d408ccdd01612043fbe07b
Giovana <giovanadutramaga@gmail.com>
2025-08-11 11:04:57 -0300

    Cadastro da pizza e bebida, admin

--- backend django/bd/admin.py
+++ backend django/bd/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Pizza, Drink, Admin, Customer
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name', 'ingredients')
+
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Drink, DrinkAdmin)
+admin.site.register(Admin, AdminAdmin)
+
 
 # Register your models here.
