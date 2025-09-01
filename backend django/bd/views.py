5f3e1f08458e545f56d06d552d0032e1c3eeb559
Giovana <giovanadutramaga@gmail.com>
2025-09-01 11:57:23 -0300

    mesclagem do urls e views

--- backend django/bd/views.py
+++ backend django/bd/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CustomerForm, AdminForm, LoginForm
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'pizzaria-app/home.html')
+
+def cliente_opcoes(request):
+    return render(request, 'pizzaria-app/cliente_opcoes.html')
+
+def cadastro_cliente(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CustomerForm()
+    return render(request, 'pizzaria-app/cadastro_cliente.html', {'form': form})
+
+def cadastro_admin(request):
+    if request.method == 'POST':
+        form = AdminForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.is_staff = True
+            user.save()
+            return redirect('home')
+    else:
+        form = AdminForm()
+    return render(request, 'pizzaria-app/cadastro_admin.html', {'form': form})
+
+def login_customer(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, "Usuário ou senha inválidos.")
+    else:
+        form = LoginForm()
+    return render(request, 'pizzaria-app/login_customer.html', {'form': form})
--- backend django/pizzaria/urls.py
+++ backend django/pizzaria/urls.py
@@ -21,4 +21,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pizzaria_app.urls')),
+    path('', views.home, name='home'),
+    path('cadastro/customer/', views.cliente_opcoes, name='cliente_opcoes'),
+    path('cadastro/customer/cadastrar/', views.cadastro_cliente, name='cadastro_cliente'),
+    path('cadastro/admin/', views.cadastro_admin, name='cadastro_admin'),
+    path('cadastro/customer/entrar/', views.login_customer, name='login_customer'),
+
 ]
